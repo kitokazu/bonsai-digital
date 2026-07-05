586614d7971a5c615aad4307964383547df49c68
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2026-07-06 00:24:41 +0900

    switched order

--- src/lib/work.ts
+++ src/lib/work.ts
@@ -21,6 +21,31 @@ export interface WorkProject {
 
 export const workProjects: WorkProject[] = [
   {
+    id: "influencerAgency",
+    bucket: "software",
+    image: "/uncharted/dashboard.png",
+    slug: "influencer-platform",
+  },
+  {
+    id: "cgOnlineAcademy",
+    bucket: "websites",
+    image: "/cg-landing.png",
+    slug: "cg-online-academy",
+  },
+  {
+    id: "publicSectorAI",
+    bucket: "software",
+    confidential: true,
+    image: "/public-sector/main-page.png",
+    slug: "public-sector-ai",
+  },
+  {
+    id: "definex",
+    bucket: "websites",
+    image: "/define-x-about.png",
+    slug: "definex",
+  },
+  {
     id: "homeHairCoffee",
     bucket: "websites",
     image: "/home-hair-coffee/home-hair-landing.png",
@@ -31,31 +56,6 @@ export const workProjects: WorkProject[] = [
     bucket: "websites",
     image: "/chnl301/chnl301-landing.png",
     slug: "chnl301",
-  },
-  {
-    id: "cgOnlineAcademy",
-    bucket: "websites",
-    image: "/cg-landing.png",
-    slug: "cg-online-academy",
-  },
-  {
-    id: "definex",
-    bucket: "websites",
-    image: "/define-x-about.png",
-    slug: "definex",
-  },
-  {
-    id: "influencerAgency",
-    bucket: "software",
-    image: "/uncharted/dashboard.png",
-    slug: "influencer-platform",
-  },
-  {
-    id: "publicSectorAI",
-    bucket: "software",
-    confidential: true,
-    image: "/public-sector/main-page.png",
-    slug: "public-sector-ai",
   },
   {
     id: "arGalleryMVP",
